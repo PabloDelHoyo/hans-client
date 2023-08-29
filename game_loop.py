7f0b3bb3c0f6592c011c9005c2a07355bc3bbe56
Pablo del Hoyo Abad <pablo.hoyo@alu.uclm.es>
2023-08-29 14:27:49 +0200

    Refactor: separate platform and client code

---
+++ game_loop.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+# https://gafferongames.com/post/fix_your_timestep/
+# https://gameprogrammingpatterns.com/game-loop.html
+
+# In those articles it is recommended to interpolete between two frames. For this application,
+# I don't think it is worth the trouble. The interpolation solves two problems.
+#   1. If the fps is greater than the tps, then the movement is stuttered. The interpolation
+#      helps to make smoother. Therefore, this might be useful for the frontend altough, again,
+#      it is not the trouble
+#
+#   2. Because the updating and "rendering" happen at different rates, there will usually be
+#      the same number of updates between two renders (assume tps > fps ie. 60 and 50). However
+#      the state we should draw is not the one which is indicated by the simulation but
+#      the one of the few ms which separate the last update and the render. Aditionally,
+#      these remains accumulate which have the effect of performing more simulations between
+#      renders so it gives the impression that the simulation has run faster. This problem
+#      can be avoided with lerp.
+
+import threading
+import time
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from client import HansClient, Round
+
+
+class GameLoopThread(threading.Thread):
+    def __init__(self, brain_cls, fps=20, tps=20, brain_kwargs={}):
+        super().__init__()
+
+        self.fps = fps
+        self.tps = tps
+
+        self._brain_cls = brain_cls
+        self._brain_kwargs = brain_kwargs
+
+        self._current_hans_client = None
+
+        self._max_frame_time = 1 / fps
+        self._delta = 1 / tps
+
+        # A boolean flag would have been enough but that assumes that
+        # we are working with a Python implementation which uses the GIL.
+        # Thererfore, event is more robust
+        self._current_loop_quit = threading.Event()
+        self._continue = threading.Event()
+        self._thread_quit = threading.Event()
+
+        # These will be set when a new loop is called
+        self._current_brain = None
+
+    def stop(self):
+        """Stops and clears the currently executing game loop. This implies that the
+        state of the game loop is lost (but not the one of the brain).
+        This method must be called before calling new loop."""
+
+        self._current_loop_quit.set()
+        self._continue.clear()
+
+    def new_loop(self, round: Round, hans_client: HansClient):
+        """Creates a new game loop in this thread for the given round.
+        A new instance of the brain will be created.
+        You have to call this method in order for a game loop to run. Otherwise, the thread
+        will stay idle.
+        """
+
+        self._current_brain = self._brain_cls(round=round, **self._brain_kwargs)
+        self._current_hans_client = hans_client
+
+        self._continue.set()
+        self._current_loop_quit.clear()
+
+    def quit(self):
+        """Exits the thread"""
+
+        self._thread_quit.set()
+        self._current_loop_quit.set()
+        self._continue.set()
+
+    def run(self):
+        while self._continue.wait():
+            if self._thread_quit.is_set():
+                break
+
+            self._run_loop()
+
+    def _run_loop(self):
+        current_time = time.monotonic()
+        accumulator = 0
+
+        while not self._current_loop_quit.is_set():
+            new_time = time.monotonic()
+            frame_time = new_time - current_time
+            current_time = new_time
+
+            accumulator += frame_time
+            # In the article, the frame time is upper bounded (0.25ms). I think
+            # this is to avoid
+            while accumulator >= self._delta and not self._current_loop_quit.is_set():
+                self._current_brain.update(self._delta)
+                accumulator -= self._delta
+
+            if not self._current_loop_quit.is_set():
+                self._current_brain.render(
+                    self._current_hans_client, accumulator / self._delta
+                )
+
+            remaining_frame_time = self._max_frame_time - (
+                time.monotonic() - current_time
+            )
+            if remaining_frame_time > 0:
+                # We employ Event instead of time.sleep() because in that way,
+                # if stop() or quit() is called then this thread can exit as soon as
+                # the scheduler decides
+                self._current_loop_quit.wait(remaining_frame_time)
--- position_codec.py
+++ position_codec.py
@@ -1,43 +1,46 @@
 import numpy as np
 
+
 class PositionCodec:
-    """ Useful to transform the position from and to the format which the
-    server requires """
+    """Useful to transform the position from and to the format which the
+    server requires"""
 
     def __init__(self, num_answers: int, radius=430):
         self.radius = radius
 
         # Keep in mind that the basis vector for the y axis points downwards. Therefore,
         # in this case, the first response will be drawn upwards (at pi / 2)
-        angles = np.linspace(-np.pi / 2, -np.pi / 2 + 2 * np.pi, num_answers, endpoint=False)
+        angles = np.linspace(
+            -np.pi / 2, -np.pi / 2 + 2 * np.pi, num_answers, endpoint=False
+        )
 
         # We truncate because that is what is done in the original
         # (num_answers, 2)
-        self.answer_points = (
-            np.trunc(radius * np.stack((np.cos(angles), np.sin(angles)), axis=1))
+        self.answer_points = np.trunc(
+            radius * np.stack((np.cos(angles), np.sin(angles)), axis=1)
         )
-    
+
     def distance_to_answers(self, point: np.array) -> np.array:
-        """ Calculates the distance of point to every answer. 
-        Point must be a (2, ) shape numpy array """
+        """Calculates the distance of point to every answer.
+        Point must be a (2, ) shape numpy array"""
 
         diff_sq = (self.answer_points - point) ** 2
         return np.sqrt(diff_sq.sum(axis=1))
-    
-    def encode(self, point: np.array) -> np.array:
-        """ Transforms the (2, ) numpy array into another numpy array in the format 
-        required by the hans platform """
 
-        sort_indices = np.argsort(self.distance_to_answers(point))[:2]
-        new_basis = self.answer_points[sort_indices]
+    def encode(self, point: np.array) -> np.array:
+        """Transforms the (2, ) numpy array into another numpy array in the format
+        required by the hans platform"""
+
+        closest_indices = np.argsort(self.distance_to_answers(point))[:2]
+        new_basis = self.answer_points[closest_indices]
         point_new_basis = np.linalg.solve(new_basis.T, point)
 
         encoded_position = np.zeros(self.answer_points.shape[0])
-        encoded_position[sort_indices] = point_new_basis
+        encoded_position[closest_indices] = point_new_basis
 
         return encoded_position
 
     def decode(self, encoded_position: np.array) -> np.array:
-        """ Decodes the format sent by the hans platform """
+        """Decodes the format sent by the hans platform"""
 
         return self.answer_points.T @ encoded_position
